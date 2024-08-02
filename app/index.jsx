85222b19821a0a12d920dd14eee4ed968079824d
owaisazmal <owaiskhan461@gmail.com>
2024-08-02 13:28:36 -0700

    NavBar updated

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
 
 export default function App() {
   return (
-    <View className ="flex-1 items-center justify-center bg-white">
-      <Text className ="text-5xl font-pextrabold">Hello World!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color:'blue', textDecoration: 'underline'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className = "bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image 
+          source={images.logo}
+          className="w-[130px] h-[80px]"
+          resizeMode="contain"
+          />
+          <Image 
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode="contain"
+          />
+          <View>
+            <Text className="relative mt-5 font-bold text-center">
+            Discover Endless Possibilities
+            </Text>
+          </View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
