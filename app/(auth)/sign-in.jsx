693e62397fbcc325101f5ee34429efdc6914ca54
owaisazmal <owaiskhan461@gmail.com>
2024-08-12 23:53:49 -0700

    signup sheet

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -59,7 +59,7 @@ const SignIn = () => {
               href="/sign-up"
               className="text-lg font-psemibold text-secondary"
             >
-              Signup
+              Sign up
           </Link>
         </View>
         </View>
